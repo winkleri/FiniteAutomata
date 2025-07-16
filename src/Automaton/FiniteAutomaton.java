44cdefa9c087080b21c78a7f806b06b2c0e07d48
winkleri <iljawinkler@googlemail.com>
2025-07-17 00:08:04 +0200

    Removed redundant code

--- src/Automaton/FiniteAutomaton.java
+++ src/Automaton/FiniteAutomaton.java
@@ -6,21 +6,17 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class FiniteAutomaton implements Automaton {
-    //HashSet<Character> sigma;
-    // NFA HashSet<State> currentStates;
     private final State dead = new State("dead");
     private final Scanner sc;
     private final String name;
     private State start;
-    State current; //DFA
-    State win;
-    HashSet<State> states;
-    HashSet<State> accepting;
+    private State current;
+    private State win;
+    private final HashSet<State> accepting;
 
     public FiniteAutomaton(String name) {
         this.name = name;
         this.start = new State("start");
-        this.states = new HashSet<>();
         this.sc = new Scanner(System.in);
         this.accepting = new HashSet<>();
         initialize();
@@ -29,26 +25,21 @@ public class FiniteAutomaton implements Automaton {
     public void initialize() {
         State q0 = new State("q0");
         State q1 = new State("q1");
-        Transition t1 = new Transition(q0, q0, '0');
-        Transition t2 = new Transition(q0, q1, '1');
-        Transition t3 = new Transition(q1, q1, 'a', 'b', 'c');
+        Transition t1 = new Transition(q0, '0');
+        Transition t2 = new Transition(q1, '1');
+        Transition t3 = new Transition(q1, 'a', 'b', 'c');
         q0.addEdge(t1,t2); //q0 calls method .addEdge() because it is an origin state for t1 and t2
         q1.addEdge(t3); // t3 loops on q1
         start = q0;
         win = q1;
         accepting.add(win);
-        states.add(start);
-        states.add(current);
-        states.add(dead);
-        states.add(win);
     }
-
-
 
     public void run() {
         current = start;
         System.out.printf("Enter word for state machine %s\n", name);
         String input = sc.nextLine();
+        sc.close();
         current = transition(input);
 
         if(current.equals(win)) {
@@ -63,7 +54,7 @@ public class FiniteAutomaton implements Automaton {
             current = win; //win is a state that marks when satisfactory conditions for acceptance are met
             return current;
         } else if(word.isEmpty()) {
-            current = dead; //dead state always rejects
+            current = dead;
             return current;
         }
         for(Transition transition : current.getEdges()) {
--- src/Component/Transition.java
+++ src/Component/Transition.java
@@ -1,25 +1,20 @@
 package Component;
 
 public class Transition {
-    char[] letters = new char[5];
-    State origin;
-    State destination;
-    //        Currently no use case for method found since transition handles loops already
-//        public boolean isLoop() {
-//            return origin.equals(destination);
-//        }
-    public Transition(State origin, State destination, char... chars) {
-        this.origin = origin;
+    //modifiable amount of letters
+    private final char[] letters = new char[5];
+    private final State destination;
+
+    public Transition(State destination, char... chars) {
         this.destination = destination;
         //copy varargs into array
-        for (int i = 0; i < chars.length; i++) {
-            letters[i] = chars[i];
-        }
+        System.arraycopy(chars, 0, letters, 0, chars.length);
     }
 
     public State getDestination() {
         return this.destination;
     }
+
     public char[] getTransitionLetters() {
         return letters.clone();
     }
